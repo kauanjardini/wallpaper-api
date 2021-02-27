8329a05051d7010906af47c0d653a76642bbf2d9
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-27 09:58:57 +0700

    Update index.js

--- api/islamic/index.js
+++ api/islamic/index.js
@@ -1,9 +1,10 @@
 const path = require('path')
 const fs = require('fs').promises
-const fileJson = path.join(__dirname, '..', '..', '..', 'data', 'Islamic.json')
+const fileJson = path.join(__dirname, '..', '..', 'data', 'Islamic.json')
 
 module.exports = async (_, res) => {
     const data = await fs.readFile(fileJson, 'utf-8')
     const wallpaper = JSON.parse(data)
-    res.json({ status : true, results : wallpaper})
+
+    res.status(200).send(wallpaper)
 }
